3e33b835d920a2861633540b18373df3162363c0
skymim <ppt00003333@gmail.com>
2023-06-28 10:45:12 +0900

    lol

--- src/main/kotlin/kr/skymin/warp/event/WarpTeleportEvent.kt
+++ src/main/kotlin/kr/skymin/warp/event/WarpTeleportEvent.kt
@@ -2,15 +2,30 @@ package kr.skymin.warp.event
 
 import kr.skymin.warp.manager.Warp
 import org.bukkit.entity.Player
+import org.bukkit.event.Cancellable
 import org.bukkit.event.HandlerList
 
-class WarpTeleportEvent(val player: Player, warp: Warp) : WarpEvent(warp) {
+class WarpTeleportEvent(val player: Player, warp: Warp) : WarpEvent(warp), Cancellable {
 	companion object {
 		@JvmStatic
 		val handlerList = HandlerList()
 	}
 
+	private var isCancelled = false
+
 	override fun getHandlers(): HandlerList {
 		return handlerList
+	}
+
+	override fun isCancelled(): Boolean {
+		return isCancelled
+	}
+
+	fun setCancelled() {
+		setCancelled(true)
+	}
+
+	override fun setCancelled(cancel: Boolean) {
+		isCancelled = cancel
 	}
 }
